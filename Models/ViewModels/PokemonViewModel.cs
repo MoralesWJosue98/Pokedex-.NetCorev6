e54b9c09e2f1ad7c59491f19f4ade5ffb61e9f53
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-08 18:05:36 -0600

    Add: Pokemon ViewModel :technologist:

---
+++ Models/ViewModels/PokemonViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Pokedex.Models.ViewModels
+{
+    public class PokemonViewModel
+    {
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String ImgUrl { get; set; }
+        public int PrimaryType { get; set; }
+        public int SecondaryType { get; set; }
+        public int RegionId { get; set; }
+        public int Id { get; set; }
+    }
+}
