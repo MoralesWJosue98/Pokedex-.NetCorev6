dc7a49cc941e2c264c4711441f7c3e00a75ccd16
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-08 18:53:42 -0600

    Add: Pokemon Type Service :technologist:

--- Repository/PokemonTypeRepository.cs
+++ Repository/PokemonTypeRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Pokedex.Models;
+using Type = Pokedex.Models.Type;
 
 namespace Pokedex.Repository
 {
@@ -13,39 +14,39 @@ namespace Pokedex.Repository
         }
 
         // Add Pokemon Type
-        public async Task AddAsync(Pokemon pokemon)
+        public async Task AddAsync(Type pokemonType)
         {
-            await _dbContext.Set<Pokemon>().AddAsync(pokemon);
+            await _dbContext.Set<Type>().AddAsync(pokemonType);
             await _dbContext.SaveChangesAsync();
         }
 
         // Update Pokemon Type
-        public async Task UpdateAsync(Pokemon pokemon)
+        public async Task UpdateAsync(Type pokemonType)
         {
-            _dbContext.Entry(pokemon).State = EntityState.Modified;
+            _dbContext.Entry(pokemonType).State = EntityState.Modified;
             await _dbContext.SaveChangesAsync();
         }
 
         // Delete Pokemon Type
-        public async Task DeleteAsync(Pokemon pokemon)
+        public async Task DeleteAsync(Type pokemonType)
         {
-            _dbContext.Set<Pokemon>().Remove(pokemon);
+            _dbContext.Set<Type>().Remove(pokemonType);
             await _dbContext.SaveChangesAsync();
         }
 
         // Get all Pokemons Type
-        public async Task<List<Pokemon>> GetAllAsync()
+        public async Task<List<Type>> GetAllAsync()
         {
             return await _dbContext
-                .Set<Pokemon>()
+                .Set<Type>()
                 .ToListAsync();
         }
 
         // Get Pokemon Type by ID
-        public async Task<Pokemon> GetByIdAsync(int id)
+        public async Task<Type> GetByIdAsync(int id)
         {
             return await _dbContext
-                .Set<Pokemon>()
+                .Set<Type>()
                 .FindAsync(id);
         }
 
---
+++ Service/TypeService.cs
@@ -0,0 +1,68 @@
+﻿using Pokedex.Models;
+using Pokedex.Models.ViewModels;
+using Pokedex.Repository;
+using Type = Pokedex.Models.Type;
+
+namespace Pokedex.Service
+{
+    public class TypeService
+    {
+        private readonly PokemonTypeRepository _repository;
+
+        public TypeService(ApplicationDbContext dbContext)
+        {
+            _repository = new(dbContext);
+        }
+
+        public async Task AddTypeService(SaveTypeViewModel savePk)
+        {
+            Type pkType = new()
+            {
+                Id = savePk.Id,
+                Name = savePk.Name
+            };
+
+            await _repository.AddAsync(pkType);
+        }
+
+        public async Task updateTypeService(SaveTypeViewModel saveType)
+        {
+            Type pkType = new()
+            {
+                Id = saveType.Id,
+                Name = saveType.Name
+            };
+
+            await _repository.UpdateAsync(pkType);
+        }
+
+        public async Task deleteTypeService(int id)
+        {
+            var pkType = await _repository.GetByIdAsync(id);
+            await _repository.DeleteAsync(pkType);
+        }
+        
+        public async Task<SaveTypeViewModel> getTypeyIdService(int id)
+        {
+            var pkType = await _repository.GetByIdAsync(id);
+            SaveTypeViewModel savePk = new()
+            {
+                Id = pkType.Id,
+                Name = pkType.Name
+            };
+            return savePk;
+        }
+
+        public async Task<List<SaveTypeViewModel>> getAllTypeService()
+        {
+            var list = await _repository.GetAllAsync();
+            return list.Select(s => new SaveTypeViewModel
+            {
+                Name = s.Name,
+                Id = s.Id
+
+            }).ToList();
+        }
+
+    }
+}
