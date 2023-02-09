d0106e5a84e0aa79847e1fd8ed85bae08bf5c26a
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-08 18:12:04 -0600

    Modify: Pokemon View Model :pencil2:

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -4,20 +4,15 @@ namespace Pokedex.Models
 {
     public class Pokemon
     {
-        [Required(ErrorMessage = "Colocar Nombre del Pokemon !")]
         public String Name { get; set; }
 
-        [Required(ErrorMessage = "Debe colocar la descripción del pokemon !")]
         public String Description { get; set; }
 
-        [Required(ErrorMessage = "Debe colocar la URL del pokemon !")]
         public String ImgUrl { get; set; }
 
-        [Required(ErrorMessage = "**")]
         public int PrimaryType { get; set; }    
         public int SecondaryType { get; set;}
 
-        [Required(ErrorMessage = "**")]
         public int RegionID { get; set; }
         public int Id { get; set; } 
 
---
+++ Models/ViewModels/SavePokemonViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pokedex.Models.ViewModels
+{
+    public class SavePokemonViewModel
+    {
+        [Required(ErrorMessage = "Colocar Nombre del Pokemon !")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar la descripción del pokemon !")]
+        public String Description { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar la URL del pokemon !")]
+        public String ImgUrl { get; set; }
+
+        [Required(ErrorMessage = "**")]
+        public int PrimaryType { get; set; }
+
+        public int SecondaryType { get; set; }
+
+        [Required(ErrorMessage = "**")]
+        public int RegionId { get; set; }
+        public int Id { get; set; }
+    }
+}
